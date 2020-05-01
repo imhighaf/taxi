9ec87e9a058a50fa5068cba92aa4c2ec65862809
Oleksandr Hradiushko <oleksandrhradiushko@Oleksandrs-Mac-mini.local>
2020-05-01 19:37:03 +0300

    auth

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
+const mid = (req,res, next) => {
+    console.log(req.session)    
+    next();
+}
 
-app.get('/', (req, res) => {
-    res.send({hi: 'Zdorov, Pavlo)))'})
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+
+app.use(mid)
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/auth')(app);
+
+
 
 const PORT = process.env.PORT || 5000;
 
