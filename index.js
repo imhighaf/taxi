7837f01b15fd9ef90589b806d31fbef2f7b83b31
Oleksandr Hradiushko <oleksandrhradiushko@Oleksandrs-Mac-mini.local>
2020-05-02 14:55:42 +0300

    client

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ if (process.env.NODE_ENV === 'produnction') {
     app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
+        //res.send({hi: 'hi'})
         res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
     })
 }
