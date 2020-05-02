c46b7a5b9dfe9f6334eb63e2d5c6aba809df53bc
Oleksandr Hradiushko <oleksandrhradiushko@Oleksandrs-Mac-mini.local>
2020-05-02 15:15:10 +0300

    client

--- index.js
+++ index.js
@@ -24,14 +24,17 @@ app.use(passport.session());
 require('./routes/auth')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
     const path = require('path');
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
     app.get('*', (req, res) => {
        // res.send({hi: 'hi'})
-        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     })
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
+//"homepage": "http://taxi-dyapchiki.herokuapp.com/client/build",
