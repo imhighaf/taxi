193d9eae555ab547dc2b8e1a3730d508d522bd4b
Oleksandr Hradiushko <oleksandrhradiushko@Oleksandrs-Mac-mini.local>
2020-05-02 15:08:40 +0300

    client

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.use(passport.session());
 
 require('./routes/auth')(app);
 
-if (process.env.NODE_ENV === 'produnction') {
-    //app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
-        res.send({hi: 'hi'})
-        //res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+       // res.send({hi: 'hi'})
+        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
     })
 }
 
