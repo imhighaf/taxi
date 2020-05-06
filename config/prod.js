2526d9f8092cce69a26621d26b56fa70c2141e0f
Oleksandr Hradiushko <oleksandrhradiushko@Oleksandrs-Mac-mini.local>
2020-05-06 22:49:47 +0300

    cars from api

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,6 @@ module.exports = {
     cookieKey: process.env.COOKIE_KEY,
     //google spreadsheets
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     googleSheetId: process.env.GOOGLE_SHEET_ID
 }
